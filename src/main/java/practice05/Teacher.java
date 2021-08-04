ff38158632bebfe7fd8fe557a91b9bbf767a19e2
dyje <dyje@oocl.com>
2021-08-04 20:08:05 +0800

    [JavaOOP_Practice][Jesse] feat: completed Practice05

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,22 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    public int getKlass() {
+        return klass;
+    }
+    private int klass;
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+    public String introduce(){
+        if (klass == 0){
+            return super.introduce() + " I am a Teacher. " + "I teach No Class.";
+        }else
+        return super.introduce() + " I am a Teacher. " + "I teach Class " + klass + ".";
+    }
 }
