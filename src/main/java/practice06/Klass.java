3539bffa19a3f93c8cd61ea11dc5f3235979fb52
dyje <dyje@oocl.com>
2021-08-04 22:00:38 +0800

    [JavaOOP_Practice][Jesse] feat: updated Klass.java in Practice06

--- src/main/java/practice06/Klass.java
+++ src/main/java/practice06/Klass.java
@@ -1,4 +1,16 @@
 package practice06;
 
+import java.text.MessageFormat;
+
 public class Klass {
+    private int num;
+    public Klass(int num) {
+        this.num = num;
+    }
+    public int getNumber() {
+        return num;
+    }
+    public String getDisplayName() {
+        return MessageFormat.format("Class {0}", num);
+    }
 }
