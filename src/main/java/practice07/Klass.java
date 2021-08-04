263052e9fd9c522ab68db5b69ac385e3f68a0d2c
dyje <dyje@oocl.com>
2021-08-04 23:05:09 +0800

    [JavaOOP_Practice][Jesse] feat: updated Klass.java in Practice07

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,26 @@
 package practice07;
 
+import java.text.MessageFormat;
+
 public class Klass {
+    private final int num;
+
+    public Student getLeader() {
+        return leader;
+    }
+    private Student leader;
+
+    public Klass(int num) {
+        this.num = num;
+    }
+    public int getNumber() {
+        return num;
+    }
+    public String getDisplayName() {
+        return MessageFormat.format("Class {0}", num);
+    }
+
+    public void assignLeader(Student student){
+        this.leader = student;
+    }
 }
