3a80cb0e71cf8bf4f676928d687761b0389cfd72
dyje <dyje@oocl.com>
2021-08-04 22:04:39 +0800

    [JavaOOP_Practice][Jesse] refactor: refactored code in Teacher.java

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -2,8 +2,6 @@ package practice06;
 
 import practice05.Person;
 
-import java.text.MessageFormat;
-
 public class Teacher extends Person {
 
     public Klass getKlass() {
@@ -29,19 +27,9 @@ public class Teacher extends Person {
     public String introduceWith(Student student){
         String teacher = super.introduce() + " " + "I am a Teacher. ";
         if (student.getKlass().getNumber() == (this.getKlass().getNumber())) {
-            return MessageFormat.format("{0}" + "I teach {1}.",teacher, student.getName());
+            return String.format(teacher + "I teach %s.", student.getName());
         }else{
-            return MessageFormat.format("{0}" + "I don't teach {1}.",teacher, student.getName());
+            return String.format(teacher + "I don't teach %s.", student.getName());
         }
     }
-
-//    String teacherIntro = super.introduce() + " " + "I am a Teacher. ";
-//    String studentName = student.getName();
-//    return student.getKlass().getNumber().equals(this.getKlass().getNumber()) ?
-//    teacherIntro + String.format("I teach %s.", studentName) : teacherIntro + String.format("I don't teach %s.", studentName);
-//
-//    "My name is Tom. I am 21 years old. I am a Teacher. I teach Jerry."
-//
-//            //return MessageFormat.format("My name is {0}. I am {1} years old.", name, age);
-//            //String.format("My name is %s. I am %d years old.", name, age);
 }
