272ce2911989da4c6cd14c0b4f2e1ee7c041dfbe
dyje <dyje@oocl.com>
2021-08-04 16:38:36 +0800

    [JavaOOP_Practice][Jesse] feat: completed Practice04 of test case 1

--- src/main/java/practice04/Person.java
+++ src/main/java/practice04/Person.java
@@ -1,4 +1,31 @@
 package practice04;
 
 public class Person {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    String introduce (){
+        return "Hi my name is " + name + "I am " + age + " years old.";
+    }
 }
