76360e17f099cc5ffb29cfab21a3519a7ffb6980
dyje <dyje@oocl.com>
2021-08-04 22:01:13 +0800

    [JavaOOP_Practice][Jesse] feat: updated Student.java in Practice06

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -4,17 +4,17 @@ import practice05.Person;
 
 public class Student extends Person {
 
-    public int getKlass() {
+    public Klass getKlass() {
         return klass;
     }
 
-    private int klass;
-    public Student(String name, int age, int klass) {
+    private Klass klass;
+    public Student(String name, int age, Klass klass) {
         super(name, age);
         this.klass = klass;
     }
 
     public String introduce(){
-        return super.introduce() + " I am a Student. I am at Class " + klass + ".";
+        return super.introduce() + " I am a Student. I am at Class " + klass.getNumber() + ".";
     }
 }
