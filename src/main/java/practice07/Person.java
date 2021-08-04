66dd8c999019fb419dafd4d92b99fe6ecd54fd06
dyje <dyje@oocl.com>
2021-08-04 22:19:54 +0800

    [JavaOOP_Practice][Jesse] feat: updated Person.java in Practice07

--- src/main/java/practice07/Person.java
+++ src/main/java/practice07/Person.java
@@ -1,4 +1,38 @@
 package practice07;
 
 public class Person {
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String name;
+    public int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public int id;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public boolean equals(Object object){
+        if (object instanceof Person){
+            return ((Person) object).getId() == this.getId();
+        }
+        return false;
+    }
+
+    public String introduce (){
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
 }
