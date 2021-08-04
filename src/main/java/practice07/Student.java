073889bd03fbe93ab246ebd3c7c84e6217a880b3
dyje <dyje@oocl.com>
2021-08-04 23:10:46 +0800

    [JavaOOP_Practice][Jesse] feat: updated Student.java in Practice07

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,18 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    private Klass klass;
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        return super.introduce() + " I am a Student. I am at Class " + klass.getNumber() + ".";
+    }
 }
