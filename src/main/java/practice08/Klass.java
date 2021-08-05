7acc94a21794c9336bd1b9cfd19024eaee978108
dyje <dyje@oocl.com>
2021-08-05 19:21:56 +0800

    [JavaOOP_Practice08][Jesse] feat: Implementation for test cases:
    should_class_have_a_number()
    should_class_get_display_name()
    should_class_not_assign_a_student_as_leader_when_student_is_not_a_member()

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,38 @@
 package practice08;
 
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+
+    private final int num;
+    private Student leader;
+    List<Student> students = new ArrayList<>();
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public Klass(int num) {
+        this.num = num;
+    }
+
+    public int getNumber() {
+        return num;
+    }
+
+    public String getDisplayName() {
+        return MessageFormat.format("Class {0}", num);
+    }
+
+    public void assignLeader(Student leader) {
+        if (students.contains(leader)){
+            this.leader = leader;
+        }
+        else{
+            System.out.print("It is not one of us.\n");
+        }
+    }
 }
