79bf8f4db4061aa36381727fdcfe5e0bcee05e0b
dyje <dyje@oocl.com>
2021-08-05 19:33:06 +0800

    [JavaOOP_Practice08][Jesse] feat: Implementation for test cases:
    should_teacher_have_name_and_age_and_class()
    should_teacher_introduce_itself_with_which_class_it_teaches()
    should_teacher_introduce_itself_with_no_class_teaching()
    should_teacher_introduce_a_student_it_teaches()
    should_teacher_introduce_a_student_it_does_not_teach()

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,36 @@
 package practice08;
 
-public class Teacher {
+
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String teacher = super.introduce() + " " + "I am a Teacher. ";
+
+    public String introduce() {
+        if (klass != null)
+            return String.format(teacher + "I teach Class %s.", klass.getNumber());
+        else
+            return teacher + "I teach No Class.";
+    }
+
+    public String introduceWith(Student student) {
+        if (student.getKlass().getNumber() == klass.getNumber()) {
+            return String.format(teacher + "I teach %s.", student.getName());
+        } else
+            return String.format(teacher + "I don't teach %s.", student.getName());
+
+    }
 }
