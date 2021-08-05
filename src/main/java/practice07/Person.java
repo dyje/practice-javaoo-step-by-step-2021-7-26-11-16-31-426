f340479efdcf8385caf8c2f3cc6b0a027c06a3c7
dyje <dyje@oocl.com>
2021-08-05 18:55:18 +0800

    [JavaOOP_Practice07][Jesse] refactor: refactor codes

--- src/main/java/practice07/Person.java
+++ src/main/java/practice07/Person.java
@@ -1,6 +1,40 @@
 package practice07;
 
+import java.util.Objects;
+
 public class Person {
+
+    public String name;
+    public int age;
+    public int id;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+
+    }
+
+    public String introduce() {
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
+
+    public int getId() {
+        return id;
+    }
 
     public String getName() {
         return name;
@@ -10,29 +44,5 @@ public class Person {
         return age;
     }
 
-    public String name;
-    public int age;
 
-    public int getId() {
-        return id;
-    }
-
-    public int id;
-
-    public Person(int id, String name, int age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-    }
-
-    public boolean equals(Object object){
-        if (object instanceof Person){
-            return ((Person) object).getId() == this.getId();
-        }
-        return false;
-    }
-
-    public String introduce (){
-        return "My name is " + name + ". I am " + age + " years old.";
-    }
 }
