876fd8c2e52be3a553ae24f9f35fdd0c36b34da2
dyje <dyje@oocl.com>
2021-08-07 19:11:43 +0800

    [JavaOOP_Practice09][Jesse] refactor: removed uneccessary import tags

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,7 +1,5 @@
 package practice09;
 
-import practice08.Student;
-
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,7 +7,7 @@ import java.util.List;
 public class Klass {
     private final int num;
     private Student leader;
-    List<practice08.Student> students = new ArrayList<>();
+    List<Student> students = new ArrayList<>();
 
     public Student getLeader() {
         return leader;
