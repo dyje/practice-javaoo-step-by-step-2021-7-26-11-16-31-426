2e637e026ccb5490daef8925e65650f7462e28f6
dyje <dyje@oocl.com>
2021-08-07 19:14:42 +0800

    [JavaOOP_Practice09][Jesse] feat: Implementation for test cases:
    should_student_have_name_age_and_class_number()
    should_student_introduce_with_class()
    should_student_introduce_itself_as_class_leader()

--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,29 @@
 package practice09;
 
-public class Student {
+
+public class Student extends Person {
+
+    private Klass klass;
+
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        String student = super.introduce() + " " + "I am a Student. ";
+        if (klass.getLeader() != null) {
+            return String.format(student + "I am Leader of Class %s.", klass.getNumber());
+        } else
+            return String.format(student + "I am at Class %s.", klass.getNumber());
+
+    }
 }
