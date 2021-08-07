396eae05ccf6dcc80a33b2677c967cac6ef08701
dyje <dyje@oocl.com>
2021-08-07 19:00:27 +0800

    [JavaOOP_Practice08][Jesse] feat: Implementation for test cases:
    should_person_have_id_name_and_age()
    should_person_with_same_id_be_same_one()
    should_person_have_an_introduce_method_which_introduce_person_with_name_and_age()

--- src/main/java/practice09/Person.java
+++ src/main/java/practice09/Person.java
@@ -1,4 +1,44 @@
 package practice09;
 
+import java.util.Objects;
+
 public class Person {
+    private final String name;
+    private final int age;
+    private final int id;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public String introduce() {
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
 }
